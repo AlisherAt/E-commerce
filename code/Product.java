086ca4439fa0d8f19c3499f638fbcb2390cb9d19
AlisherAt <240626@astanait.edu.kz>
2025-01-09 09:05:06 +0500

    Update Product.java

--- code/Product.java
+++ code/Product.java
@@ -1,60 +1,80 @@
-public class Product {
-    private String productName;
-    private double price;
-    private int stock;
+class Product:
+    def __init__(self, name, price):
+        self._name = name
+        self._price = price
 
-    public Product(String productName, double price, int stock) {
-        this.productName = productName;
-        this.price = price;
-        this.stock = stock;
-    }
+    def get_name(self):
+        return self._name
 
-    public String getProductName() {
-        return productName;
-    }
+    def set_name(self, name):
+        self._name = name
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
+    def get_price(self):
+        return self._price
 
-    public double getPrice() {
-        return price;
-    }
+    def set_price(self, price):
+        if price > 0:
+            self._price = price
+        else:
+            raise ValueError("Price must be positive")
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+    def display_product_details(self):
+        return f"Product Name: {self.get_name()}, Price: {self.get_price()}"
 
-    public int getStock() {
-        return stock;
-    }
 
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
+class Electronics(Product):
+    def __init__(self, name, price, brand):
+        super().__init__(name, price)
+        self._brand = brand
 
-    public void displayProduct() {
-        System.out.println("Product Details:");
-        System.out.println("Name: " + productName);
-        System.out.println("Price: $" + price);
-        System.out.println("Stock: " + stock + " units");
-    }
+    def get_brand(self):
+        return self._brand
 
-    @Override
-    public String toString() {
-        return "Product{name='" + productName + "', price=" + price + ", stock=" + stock + "}";
-    }
+    def set_brand(self, brand):
+        self._brand = brand
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Product product = (Product) obj;
-        return productName.equals(product.productName);
-    }
+    def display_product_details(self):
+        return f"Electronics Name: {self.get_name()}, Price: {self.get_price()}, Brand: {self._brand}"
 
-    @Override
-    public int hashCode() {
-        return productName.hashCode();
-    }
-}
+
+class Clothing(Product):
+    def __init__(self, name, price, size):
+        super().__init__(name, price)
+        self._size = size
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        self._size = size
+
+    def display_product_details(self):
+        return f"Clothing Name: {self.get_name()}, Price: {self.get_price()}, Size: {self._size}"
+
+
+class Furniture(Product):
+    def __init__(self, name, price, material):
+        super().__init__(name, price)
+        self._material = material
+
+    def get_material(self):
+        return self._material
+
+    def set_material(self, material):
+        self._material = material
+
+    def display_product_details(self):
+        return f"Furniture Name: {self.get_name()}, Price: {self.get_price()}, Material: {self._material}"
+
+
+def print_product_details(product):
+    print(product.display_product_details())
+
+
+shirt = Clothing("Shirt", 20.5, "M")
+sofa = Furniture("Sofa", 499.99, "Leather")
+tv = Electronics("TV", 299.99, "Sony")
+
+print_product_details(shirt)  # Polymorphism in action
+print_product_details(sofa)
+print_product_details(tv)
